99b9962463962d0b55d6a71ba2a0cd8dcdff22eb
Lea Rech <learech@hotmail.com>
2023-02-04 05:04:38 -0300

    subiendo

---
+++ index.js
@@ -0,0 +1,8 @@
+import {suma, multiplica} from './controllers/controller.js'
+import chalk from 'chalk';
+
+const sum = suma (1, 2)
+console.log(sum);
+
+const mult = multiplica (4, 5)
+console.log(chalk.green(mult));
